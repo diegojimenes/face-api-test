c89c750a094bd98415b0c9e14c9095c588dc0d4e
Diego jimenes <lligaskate@gmail.com>
2019-09-21 16:46:49 -0300

    show camera

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const video = document.getElementById("video");
+
+const startVideo = () => {
+  navigator.getUserMedia(
+    {
+      video: {}
+    },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+};
+
+startVideo();
